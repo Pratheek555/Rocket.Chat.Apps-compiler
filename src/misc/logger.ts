0e1ad0a1b1c6f37b92ff6f0c95f60b8d94c475ad
Pratheek555 <pratheekravikumar55@gmail.com>
2026-02-24 12:59:16 +0530

    missing commas

--- src/misc/logger.ts
+++ src/misc/logger.ts
@@ -2,7 +2,7 @@ import pino from "pino";
 
 const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
-    timestamp: pino.stdTimeFunctions.epochTime
-})
+    timestamp: pino.stdTimeFunctions.epochTime,
+});
 
 export default logger;
